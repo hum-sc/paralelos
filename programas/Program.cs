c551c4768642afd96f16bee5999eb01c3b426140
Humberto SC <humbertoantsc@gmai.com>
2023-02-21 22:11:28 -0600

    Configuracion de dotnet

---
+++ programas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace Program
+{
+    internal class Programa
+    {
+        static void Main(string[] args)
+        {
+            sumaERW(4294967296);
+        }
+
+        private static int sumaERW(long n){
+            //TODO: algo
+
+            for(int i = 0; i<=n; i++ ){
+                Task.Factory.StartNew(() =>{
+                    Console.WriteLine("HelloWorld");
+                }
+                );
+            }
+            Console.WriteLine("Acabo");
+            return 0;
+        }
+
+
+    }
+}
